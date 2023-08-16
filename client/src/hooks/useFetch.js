7efb4508f8af2e5ee93563a50f5d57bb316a1621
Radhi Hudijan <radhif2009@gmail.com>
2023-08-16 12:06:54 +0200

    update env variable

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -11,7 +11,7 @@ const useFetch = (url) => {
       setLoading(true);
 
       try {
-        const res = await axios.get(process.env.Heroku + url);
+        const res = await axios.get(process.env.REACT_APP_Heroku + url);
 
         setData(res.data);
       } catch (error) {
