d330940983ffa4456a566e7d4280bcd697d1314d
Radhi Hudijan <radhif2009@gmail.com>
2023-08-16 11:38:39 +0200

    update env

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -1,7 +1,5 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import dotenv from "dotenv";
-dotenv.config();
 
 const useFetch = (url) => {
   const [data, setData] = useState([]);
