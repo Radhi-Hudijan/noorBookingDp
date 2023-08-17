2bc56f4b110b90589ebb7014eaa73a42b6f46ff8
Radhi Hudijan <radhif2009@gmail.com>
2023-08-17 06:21:03 +0200

    chart are complete

--- adminpanel/src/component/normalChart/NormalChart.jsx
+++ adminpanel/src/component/normalChart/NormalChart.jsx
@@ -14,53 +14,36 @@ import {
 
 const data = [
   {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
+    name: "Jan",
+    total: 1200,
   },
   {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
+    name: "Feb",
+    total: 2100,
   },
   {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
+    name: "Mar",
+    total: 800,
   },
   {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
+    name: "Apr",
+    total: 1600,
   },
   {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
+    name: "May",
+    total: 900,
   },
   {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
+    name: "Jun",
+    total: 1700,
   },
 ];
 
 const NormalChart = () => {
   return (
     <div className="chart">
-      <ResponsiveContainer width="100%" height="100%">
+      <div className="title"> Last 6 month (Revenue)</div>
+      <ResponsiveContainer width="100%" aspect={2 / 1}>
         <AreaChart
           width={730}
           height={250}
@@ -73,26 +56,21 @@ const NormalChart = () => {
           }}
         >
           <defs>
-            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+            <linearGradient id="total" x1="0" y1="0" x2="0" y2="1">
               <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
               <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
             </linearGradient>
-            <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
-              <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
-            </linearGradient>
           </defs>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
+          <CartesianGrid strokeDasharray="3 3" className="chartGrid" />
+          <XAxis dataKey="name" stroke="gray" />
           <Tooltip />
 
           <Area
             type="monotone"
-            dataKey="uv"
+            dataKey="total"
             stroke="##8884d8"
             fillOpacity={1}
-            fill="url(#colorUv)"
+            fill="url(#total)"
           />
 
           <Area
