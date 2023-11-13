a4c729e2c5dc8a9f9aad14e6a6cc52cd1c5c9389
Radhi Hudijan <radhif2009@gmail.com>
2023-11-13 10:07:12 +0100

    update delete handler

--- adminpanel/src/component/datatable/Datatable.jsx
+++ adminpanel/src/component/datatable/Datatable.jsx
@@ -1,22 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./datatable.scss";
 import { DataGrid } from "@mui/x-data-grid";
 import { userColumns, userRows } from "../../dataTableSource";
 import { Link } from "react-router-dom";
 
 const Datatable = () => {
+
+  const [data, setData] = useState(userRows)
+
+  const handleDelete = (id) => { 
+    const newData = data.filter((row) => {
+         return row.id !== id      
+    })
+
+    setData(newData);
+  }
+  
   const actionColumn = [
     {
       field: "action",
       headerName: "Action",
       width: 200,
-      renderCell: () => {
+
+      // to get the id of row from userRows
+      renderCell: (params) => {
         return (
           <div className="cellAction">
             <Link to='/users/test' style={{ 'textDecoration': 'none' }}>
              <div className="viewButton">View</div>
             </Link>
-             <div className="deleteButton">Delete</div>
+            <div className="deleteButton" onClick={() => {
+              handleDelete(params.row.id)
+             }}>Delete</div>
           </div>
         );
       },
@@ -34,7 +49,7 @@ const Datatable = () => {
       </div>
       <DataGrid
         className="dataGrid"
-        rows={userRows}
+        rows={data}
         columns={userColumns.concat(actionColumn)}
         initialState={{
           pagination: {
