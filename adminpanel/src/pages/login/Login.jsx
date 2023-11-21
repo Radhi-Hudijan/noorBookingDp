9349ee83d2631da585b6b79e839c7e554694304c
Radhi Hudijan <radhif2009@gmail.com>
2023-11-21 10:11:50 +0100

    create admin auth

--- adminpanel/src/pages/login/Login.jsx
+++ adminpanel/src/pages/login/Login.jsx
@@ -1,7 +1,69 @@
-import "./login.scss";
+import axios from "axios";
+import React from "react";
+import { useContext } from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
+import  "./login.scss";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [credential, setCredential] = useState({
+    username: undefined,
+    password: undefined,
+  });
+
+  const { loading, error, dispatch } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  // to het the values of the inputs together
+  const handleChange = (e) => {
+    setCredential((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+
+  const handleClick = async (e) => {
+
+    // to prevent page refresh
+    e.preventDefault();
+
+    dispatch({ type: "LOGIN_START" });
+    try {
+      const res = await axios.post("api/auth/login", credential);
+      dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+      navigate("/");
+    } catch (err) {
+      dispatch({ type: "LOGIN_FAILURE", payload: err.response.data });
+    }
+  };
+
+  return (
+    <div className="login">
+      <div className="lContainer">
+        <input
+          type="text"
+          className="lInput"
+          placeholder="username"
+          id="username"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          className="lInput"
+          placeholder="password"
+          id="password"
+          onChange={handleChange}
+        />
+        <button
+          className="lButton"
+          disabled={loading}
+          onClick={handleClick}
+        >
+          Login
+        </button>
+        {error && <span>{error.message}</span>}
+      </div>
+    </div>
+  );
 };
 
 export default Login;
