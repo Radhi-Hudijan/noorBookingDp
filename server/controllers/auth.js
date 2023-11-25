ec44beb3966e7ead334adb8263b2365d586b2bf3
Radhi Hudijan <radhif2009@gmail.com>
2023-11-25 10:58:02 +0100

    update auth

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -8,8 +8,7 @@ const register = async (req, res, next) => {
     const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(req.body.password, salt);
     const newUser = new User({
-      username: req.body.username,
-      email: req.body.email,
+      ...req.body,
       password: hash,
     });
 
