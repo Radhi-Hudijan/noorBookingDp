66a70158a2c70ed190539420c9e92750552e7b9c
Radhi Hudijan <radhif2009@gmail.com>
2023-11-28 13:15:48 +0100

    adding new pages

---
+++ adminpanel/src/pages/newHotel/NewHotel.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import "./newHotel.scss";
+import Sidebar from "../../component/sidebar/Sidebar";
+import Navbar from "../../component/navbar/Navbar";
+import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
+
+const NewHotel = ({ inputs, title }) => {
+  const [file, setFile] = useState("");
+
+  return (
+    <div className="new">
+      <Sidebar />
+      
+      <div className="newContainer">
+        <Navbar />
+        <div className="top">
+          <h1>{title}</h1>
+        </div>
+        <div className="bottom">
+          <div className="left">
+            <img
+              src={
+                // creating the image file link 
+                file
+                  ? URL.createObjectURL(file)
+                  : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+              }
+              alt=""
+            />
+          </div>
+          <div className="right">
+            <form>
+              <div className="formInput">
+                <label htmlFor="file">
+                  Image: <DriveFolderUploadOutlinedIcon className="icon" />{" "}
+                </label>
+                <input
+                  type="file"
+                  id="file"
+                  style={{ display: "None" }}
+                  onChange={(e) => {
+                    setFile(e.target.files[0]);
+                  }}
+                />
+              </div>
+
+              {inputs.map((input) => {
+                return (
+                  <div className="formInput" key={input.id}>
+                    <label> {input.label} </label>
+                    <input type={input.type} placeholder={input.placeholder} />
+                  </div>
+                );
+              })}
+
+              <button>Send </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NewHotel;
